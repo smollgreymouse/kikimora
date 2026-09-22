1d75f5713001ae516ea6cc843e60bb86d7d259ca
smollgreymouse <55189163+smollgreymouse@users.noreply.github.com>
2026-09-22 18:00:39 +0300

    test(supervisor): cover unsupported interrupt fallback

--- toad/internal/supervisor/supervisor_test.go
+++ toad/internal/supervisor/supervisor_test.go
@@ -30,6 +30,35 @@ func (p *stubbornProcess) Kill() error {
 	return nil
 }
 
+type interruptUnsupportedProcess struct {
+	killed chan struct{}
+}
+
+func (p *interruptUnsupportedProcess) PID() int { return 43 }
+func (p *interruptUnsupportedProcess) Wait() error {
+	<-p.killed
+	return errors.New("killed")
+}
+func (p *interruptUnsupportedProcess) Signal(os.Signal) error {
+	return errors.New("interrupt unsupported")
+}
+func (p *interruptUnsupportedProcess) Kill() error {
+	select {
+	case <-p.killed:
+	default:
+		close(p.killed)
+	}
+	return nil
+}
+
+func TestStopAcceptsSuccessfulKillFallback(t *testing.T) {
+	process := &interruptUnsupportedProcess{killed: make(chan struct{})}
+	launcher := ExecLauncher{FinalGrace: 50 * time.Millisecond}
+	if err := launcher.Stop(context.Background(), process); err != nil {
+		t.Fatalf("Stop: %v", err)
+	}
+}
+
 func TestStopEscalatesToKillAfterGrace(t *testing.T) {
 	process := &stubbornProcess{killed: make(chan struct{})}
 	launcher := ExecLauncher{StopGrace: time.Millisecond, FinalGrace: time.Millisecond}
